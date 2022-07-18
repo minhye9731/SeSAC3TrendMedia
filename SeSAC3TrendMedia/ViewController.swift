fdf6e9553d16f78cc0fa2ee85d14a46d810df80b
minhye9731 <jenney62639731@gmail.com>
2022-07-18 09:17:26 +0900

    Initial Commit

---
+++ SeSAC3TrendMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC3TrendMedia
+//
+//  Created by 강민혜 on 7/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
