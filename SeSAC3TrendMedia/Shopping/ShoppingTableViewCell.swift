04490922e119d356276d31ae94f2c6e9608678d8
minhye9731 <jenney62639731@gmail.com>
2022-08-22 19:57:26 +0900

    체크박스 및 즐겨찾기 버튼 클릭에 따른 이미지 변경

--- SeSAC3TrendMedia/Shopping/ShoppingTableViewCell.swift
+++ SeSAC3TrendMedia/Shopping/ShoppingTableViewCell.swift
@@ -10,19 +10,49 @@ import UIKit
 class ShoppingTableViewCell: UITableViewCell {
 
     @IBOutlet weak var cellBackground: UIView!
-    @IBOutlet weak var checkBoxImage: UIImageView!
+    @IBOutlet weak var checkBoxButton: UIButton!
     @IBOutlet weak var shoppingListLabel: UILabel!
     @IBOutlet weak var bookMarkButton: UIButton!
     
+    var checkBoxButtonTapped : (() -> Void) = {}
+    var bookMarkButtonTapped : (() -> Void) = {}
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureCell()
+        configureButton()
+        configureLabel()
+    }
+    
+    func configureCell() {
         cellBackground.layer.cornerRadius = 7
         cellBackground.clipsToBounds = true
         cellBackground.backgroundColor = .systemGray6
     }
+    
+    func configureButton() {
+        checkBoxButton.setTitle("", for: .normal)
+        checkBoxButton.tintColor = .black
+        checkBoxButton.addTarget(self, action: #selector(checkBoxButtonToggle), for: .touchUpInside)
 
+        bookMarkButton.tintColor = .black
+        bookMarkButton.addTarget(self, action: #selector(bookMarkButtonToggle), for: .touchUpInside)
+    }
+    
+    func configureLabel() {
+        shoppingListLabel.font = .systemFont(ofSize: 14)
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    @objc func checkBoxButtonToggle() {
+        checkBoxButtonTapped()
+    }
+    
+    @objc func bookMarkButtonToggle() {
+        bookMarkButtonTapped()
     }
 
 }
--- SeSAC3TrendMedia/Shopping/ShoppingTableViewController.swift
+++ SeSAC3TrendMedia/Shopping/ShoppingTableViewController.swift
@@ -64,53 +64,77 @@ class ShoppingTableViewController: UITableViewController {
     // MARK: - [추가] 버튼 클릭시 액션
     @IBAction func addButtonTapped(_ sender: UIButton) {
         
-        let newList = (userTextField.text == "" ? "오늘 하루 감사하기" : userTextField.text)!
+//        let newList = (userTextField.text == "" ? "오늘 하루 감사하기" : userTextField.text)!
         
-        let task = UserShoppingList(shoppingList: newList,
-                                    regDate: Date()) // => Record
+//        let task = UserShoppingList(shoppingList: newList, regDate: Date()) // => Record
         
-        try! localRealm.write {
-            localRealm.add(task) // Create
+//        try! localRealm.write {
+//            localRealm.add(task) // (Creat) 데이터상 newList 생성
+//            tableView.reloadData()
+//            view.endEditing(true)
             
-            self.shoppingList.append(newList)
-            tableView.reloadData()
-            view.endEditing(true)
-            
-            print("Realm Succeed")
-        }
-        
-        //        shoppingList.append((userTextField.text == "" ? "장바구니 챙기기" : userTextField.text)!)
-        //        tableView.reloadData()
-        //        view.endEditing(true)
+//            print("Realm Succeed")
+//            print(tasks)
+//        }
+        addNewShoppingList()
     }
     
     @IBAction func userTextFieldTapped(_ sender: UITextField) {
-        shoppingList.append((userTextField.text == "" ? "장바구니 챙기기" : userTextField.text)!)
-        tableView.reloadData()
+//        shoppingList.append((userTextField.text == "" ? "장바구니 챙기기" : userTextField.text)!)
+//        tableView.reloadData()
+        
+        addNewShoppingList()
     }
+    
+    
+    func addNewShoppingList() {
+        let newList = (userTextField.text == "" ? "오늘 하루 감사하기" : userTextField.text)!
+        let task = UserShoppingList(shoppingList: newList, regDate: Date()) // => Record
+        
+        try! localRealm.write {
+            localRealm.add(task) // (Creat) 데이터상 newList 생성
+            tableView.reloadData()
+            view.endEditing(true)
+        }
+    }
+    
     
     // MARK: - 셀의 갯수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingList.count
-//        return tasks.count
+//        return shoppingList.count
+        return tasks.count
     }
     
     // MARK: - 셀의 UI 및 데이터 등록하기
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath) as! ShoppingTableViewCell
+        let row = tasks[indexPath.row]
+        
+        // 체크박스
+        let checkImage = row.doneCheck ? UIImage(systemName: "checkmark.square.fill") : UIImage(systemName: "checkmark.square")
+        cell.checkBoxButton.setImage(checkImage, for: .normal)
+        
+        cell.checkBoxButtonTapped = {
+            try! self.localRealm.write {
+                row.doneCheck = !row.doneCheck
+                tableView.reloadData()
+            }
+        }
         
         // 라벨 데이터 및 설정
-        cell.shoppingListLabel.text = shoppingList[indexPath.row]
-        cell.shoppingListLabel.font = .systemFont(ofSize: 14)
+        cell.shoppingListLabel.text = row.shoppingList
         
-        // 체크박스 이미지
-        cell.checkBoxImage.tintColor = .black
-        cell.checkBoxImage.image = indexPath.row % 2 == 0 ? UIImage(systemName: "checkmark.square") : UIImage(systemName: "checkmark.square.fill")
+        // 즐겨찾기 여부
+        let starImage = row.favorite ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
+        cell.bookMarkButton.setImage(starImage, for: .normal)
         
-        // 별표 버튼
-        cell.bookMarkButton.tintColor = .black
-        indexPath.row % 2 == 0 ? cell.bookMarkButton.setImage(UIImage(systemName: "star"), for: .normal) : cell.bookMarkButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        cell.bookMarkButtonTapped = {
+            try! self.localRealm.write {
+                row.favorite = !row.favorite
+                tableView.reloadData()
+            }
+        }
         
         return cell
     }
@@ -124,8 +148,12 @@ class ShoppingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete {
-            //배열삭제 후, 테이블뷰 갱신
-            shoppingList.remove(at: indexPath.row)
+            
+            try! localRealm.write {
+                localRealm.delete(tasks[indexPath.row])
+                print("삭제됨 : \(tasks[indexPath.row])")
+            }
+            
             tableView.reloadData()
         }
     }
