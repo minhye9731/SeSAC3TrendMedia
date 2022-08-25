6e9f7706e17a45ba277df5162f146fcc601ea616
minhye9731 <jenney62639731@gmail.com>
2022-08-25 14:33:43 +0900

    이미지 가져와서 보여주기 재시도

--- SeSAC3TrendMedia/extensions/FileManager+Extension.swift
+++ SeSAC3TrendMedia/extensions/FileManager+Extension.swift
@@ -10,6 +10,13 @@ import UIKit
 
 extension UIViewController {
     
+    
+    func documentDirectoryPath() -> URL? {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil } // Document 경로
+        
+        return documentDirectory
+    }
+    
     func loadImageFromDocument(fileName: String) -> UIImage? {
         guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil } // Document 경로
         let fileURL = documentDirectory.appendingPathComponent(fileName) // 세부 경로. 이미지를 저장할 위치
@@ -49,6 +56,26 @@ extension UIViewController {
         }
     }
     
+    func fetchDocumentZipFile() {
+        
+        do {
+            guard let path = documentDirectoryPath() else { return }
+            
+            let docs = try FileManager.default.contentsOfDirectory(at: path, includingPropertiesForKeys: nil)
+            
+            print("docs: \(docs)")
+            
+            let zip = docs.filter { $0.pathExtension == "zip" }
+            print("zip: \(zip)")
+            
+            let result = zip.map { $0.lastPathComponent }
+            print("result: \(result)")
+            
+        } catch {
+            print("Error")
+        }
+        
+    }
 }
 
 
